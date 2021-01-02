51ca82549c201bd5b5f550e2e8eda45c7af6feb0
Lucadmin <luca.account@gmx.de>
2021-01-03 00:50:57 +0100

    Initial commit

---
+++ Aufgabe1.c
@@ -0,0 +1,4 @@
+//
+// Created by lucaa on 02.01.2021.
+//
+
---
+++ cmake-build-debug/CMakeFiles/JonasCLion.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JonasCLion.dir/Aufgabe1.c.obj"
+  "CMakeFiles/JonasCLion.dir/main.c.obj"
+  "JonasCLion.exe"
+  "JonasCLion.exe.manifest"
+  "JonasCLion.lib"
+  "JonasCLion.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/JonasCLion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
